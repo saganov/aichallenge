0641538a50f3ad22c308dcaef7bb8abbbeea1646
Petr Saganov <saganoff@gmail.com>
2011-11-29 01:35:35 +0200

    Not Blocking Hills

--- MyBot.php
+++ MyBot.php
@@ -7,17 +7,17 @@ class MyBot
     private $ants;
 
     private $directions = array('n','e','s','w');
-    private $orders = array();
-    private $targets = array();
+    private $orders   = array();
+    private $targets  = array();
 
     // track all moves, prevent collisions
     private function do_move_direction($loc, $direction)
     {
         $new_loc = $this->ants->destination($loc, $direction);
-        if ($this->ants->unoccupied($new_loc) && !$this->inOrder($new_loc))
+        if ($this->ants->unoccupied($new_loc) && !$this->inLoc($this->orders, $new_loc))
 	{
 	    $this->ants->issueOrder($loc, $direction);
-            $this->addOrder($new_loc, $loc);
+            $this->addLoc($this->orders, $new_loc, $loc);
             return True;
 	}
         else
@@ -27,33 +27,120 @@ class MyBot
 
     }
 
-    private function inOrder($loc)
+    private function inLoc($arr, $loc)
     {
     	list($row, $col) = $loc;
-    	return (isset($this->orders[$row][$col]));
+    	return (isset($arr[$row][$col]));
     }
 
-    private function addOrder($order, $loc)
+    private function addLoc(&$arr, $order, $loc)
     {
     	list($row, $col) = $order;
-	$this->orders[$row][$col] = $loc;
+	$arr[$row][$col] = $loc;
     }
 
 
+/*
+        def do_move_location(loc, dest):
+            directions = ants.direction(loc, dest)
+            for direction in directions:
+                if do_move_direction(loc, direction):
+                    targets[dest] = loc
+                    return True
+            return False
+
+*/
+    private function do_move_location($loc, $dest)
+    {
+    	$directions = $this->ants->direction($loc, $dest);
+	foreach($directions as $direction)
+	{
+	    if($this->do_move_direction($loc, $direction))
+	    {
+	    	$this->addLoc($this->targets, $dest, $loc);
+		return TRUE;
+	    }
+	}
+	return FALSE;
+    }
+    	
+/*
+        # find close food
+        ant_dist = []
+        for food_loc in ants.food():
+            for ant_loc in ants.my_ants():
+                dist = ants.distance(ant_loc, food_loc)
+                ant_dist.append((dist, ant_loc, food_loc))
+        ant_dist.sort()
+        for dist, ant_loc, food_loc in ant_dist:
+            if food_loc not in targets and ant_loc not in targets.values():
+                do_move_location(ant_loc, food_loc)
+
+*/
     public function doTurn( $ants )
     {
-    	$this->orders = array();
-    	$this->ants = $ants;
-        foreach ( $ants->myAnts as $ant_loc )
+    	$this->orders   = array();
+    	$this->targets  = array();
+	$this->ants     = $ants;
+
+
+/*
+
+    # prevent stepping on own hill
+    for hill_loc in ants.my_hills():
+        orders[hill_loc] = None
+
+*/
+	foreach($ants->myHills as $hill_loc)
 	{
-            foreach ($this->directions as $direction)
+	    $this->addLoc($this->orders, $hill_loc, TRUE);
+	}
+
+
+	$ant_dist = array();
+	foreach($ants->food as $food_loc)
+	{
+	    foreach($ants->myAnts as $ant_loc)
 	    {
-                if ($this->do_move_direction($ant_loc, $direction))
+	    	$dist = $ants->distance($ant_loc, $food_loc);
+		$ant_dist[] = array($dist, $ant_loc, $food_loc);
+	    }
+	
+	}
+	asort($ant_dist);
+
+/*
+    # unblock own hill
+    for hill_loc in ants.my_hills():
+        if hill_loc in ants.my_ants() and hill_loc not in orders.values():
+            for direction in ('s','e','w','n'):
+                if do_move_direction(hill_loc, direction):
+                    break
+
+*/
+	foreach($ants->myHills as $hill_loc)
+	{
+	    if($this->inLoc($ants->myAnts, $hill_loc) && !in_array($hill_loc, $this->orders))
+	    {
+	    	foreach(array('s', 'e', 'w', 'n') as $direction)
 		{
-		   break;
+		    if($this->do_move_direction($hill_loc, $direction))
+		    {
+		    	break;
+		    }
 		}
-            }
-        }
+	    }
+	}
+
+
+	foreach($ant_dist as $a_dist)
+	{
+	    list($dist, $ant_loc, $food_loc) = $a_dist;
+	    if(!$this->inLoc($this->targets, $food_loc) && !in_array($ant_loc, $this->targets))
+	    {
+	    	$this->do_move_location($ant_loc, $food_loc);
+	    }
+	}
     }
     
 }
