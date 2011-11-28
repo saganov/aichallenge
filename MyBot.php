0560c66cb4440f3d364e8314f72e07b95d9581ed
Petr Saganov <saganoff@gmail.com>
2011-11-28 23:09:14 +0200

    Avoiding collisions

--- MyBot.php
+++ MyBot.php
@@ -6,16 +6,38 @@ class MyBot
 {
     private $directions = array('n','e','s','w');
 
+    private $orders = array();	
+    // track all moves, prevent collisions
+    private function do_move_direction($ants, $loc, $direction)
+    {
+	list ($aRow, $aCol) = $loc;
+        $new_loc = $ants->destination($aRow, $aCol, $direction);
+	list($nRow, $nCol) = $new_loc;
+        if ($ants->unoccupied($nRow, $nCol) && !isset($this->orders[$nRow][$nCol]))
+	{
+            $ants->issueOrder($aRow, $aCol, $direction);
+            $this->orders[$nRow][$nCol] = $loc;
+            return True;
+	}
+        else
+	{
+            return False;
+	}
+
+    }
+
+
     public function doTurn( $ants )
     {
-        foreach ( $ants->myAnts as $ant ) {
-            list ($aRow, $aCol) = $ant;
-            foreach ($this->directions as $direction) {
-                list($dRow, $dCol) = $ants->destination($aRow, $aCol, $direction);
-                if ($ants->passable($dRow, $dCol)) {
-                    $ants->issueOrder($aRow, $aCol, $direction);
-                    break;
-                }
+    	//$this->orders = array();
+        foreach ( $ants->myAnts as $ant_loc )
+	{
+            foreach ($this->directions as $direction)
+	    {
+                if ($this->do_move_direction($ants, $ant_loc, $direction))
+		{
+		   break;
+		}
             }
         }
     }
