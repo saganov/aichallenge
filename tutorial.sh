ef493081b0902b6208c692569e833b29cf2b78a4
Pyotr Saganov <pyotr.saganov@toatech.com>
2011-11-30 12:19:44 +0200

    Revert the bootstrap script do default

--- tutorial.sh
+++ tutorial.sh
@@ -1,1 +1,1 @@
-python tools/playgame.py "php MyBot.php" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 90 --scenario --food none --player_seed 7 --verbose -e
+python tools/playgame.py "php MyBot.php" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 --verbose -e
